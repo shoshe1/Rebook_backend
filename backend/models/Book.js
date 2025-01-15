ff61b5fc624bd638b1c5ffb1d3f4a358972dbe40
shoshe1 <shahdbaloum2006@gmail.com>
2025-01-15 20:37:34 +0200

    Add book schema and model for MongoDB

---
+++ backend/models/Book.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+const bookSchema = new mongoose.Schema({
+    book_id: { type: Number, required: true, unique: true },
+    book_photo: { type: String }, 
+    title: { type: String, required: true },
+    author: { type: String, required: true },
+    category: { type: String, required: true },
+    publication_year: { type: Number },
+    book_status: { type: String, enum: ['available', 'borrowed'], default: 'available' },
+    total_copies: { type: Number, required: true },
+    available_copies: { type: Number, required: true },
+  });
+  
+  module.exports = mongoose.model('Book', bookSchema);
