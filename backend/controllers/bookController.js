17134f801b8313693036a7c3409816e2bb7dbe9a
shoshe1 <shahdbaloum2006@gmail.com>
2025-01-19 14:38:33 +0200

    Add getBooks function to fetch all books in bookController.js

--- backend/controllers/bookController.js
+++ backend/controllers/bookController.js
@@ -3,3 +3,13 @@ const Book = require('../models/Book');
 const BookBorrowing = require('../models/BookBorrowing'); 
 const user = require ('../models/User')
 const BookDonation = require('../models/BookDonation');
+
+
+exports.getBooks = async (req, res) => {
+  try {
+    const books = await Book.find();
+    res.status(200).json(books);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
