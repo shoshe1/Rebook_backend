6baee2f96ed55ff522fadbd96201d6bf7953b90c
shoshe1 <shahdbaloum2006@gmail.com>
2025-01-22 16:02:29 +0200

    Add deleteUser method to delete a user by ID in userController.js

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -88,3 +88,18 @@ exports.getUsers = async (req, res) => {
             res.status(500).json({ error: error.message });
         }
     };
+    exports.deleteUser = async (req, res) => {
+        try {
+            const userId = req.params.user_id; 
+            
+            const deletedUser = await User.findOneAndDelete({ user_id: userId });
+    
+            if (!deletedUser) {
+                return res.status(404).json({ error: 'User not found' });
+            }
+    
+            res.status(200).json({ message: 'User deleted successfully' });
+        } catch (error) {
+            res.status(500).json({ error: error.message });
+        }
+    };
