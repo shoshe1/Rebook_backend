47d766878b7b6666fcbf4ab9b8fa8ff77c8d3624
shoshe1 <shahdbaloum2006@gmail.com>
2025-01-22 16:00:50 +0200

    Add getUserById method to fetch a user by ID in userController.js

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -11,3 +11,16 @@ exports.getUsers = async (req, res) => {
         res.status(500).json({ error: error.message });
     }
     };
+    exports.getUserById = async (req, res) => {
+        try {
+          const userId = req.params.user_id; 
+          const user = await User.findOne({ user_id : userId}); 
+          if (!user) {
+            res.status(404).json({ error: 'User not found', userId });
+            return;
+          }
+          res.status(200).json(user);
+        } catch (error) {
+          res.status(500).json({ error: error.message });
+        }
+      };
