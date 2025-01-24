21e3371c643a3cf4639c465506e27175d0ef2c5e
shoshe1 <shahdbaloum2006@gmail.com>
2025-01-24 13:40:58 +0200

    i fix the routes

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,11 +1,15 @@
-const {getUsers , addUser, getUserById, updateUser, deleteUser} = require('../controllers/userController');
+const {getUsers , addUser, getUserById, updateUser, deleteUser, deleteUserById} = require('../controllers/userController');
 const express = require('express');
 const router = express.Router();
+const user_controller = require('../controllers/userController');
+router.get('/', user_controller.getUsers);   
+router.get('/:user_id', user_controller.getUserById);
+router.post('/', user_controller.addUser);
+// router.put('/:id', updateUser);
+router.post('/login', user_controller.log_in);
+router.post('/logout', user_controller.log_out);
+router.delete('/:user_id', user_controller.deleteUser);
+router.get('/:user_id/donations',user_controller.user_donations_history);
+router.get('/:user_id/borrowings',ler.user_borrowing_history);
 
-router.get('/', getUsers);   
-router.get('/:id', getUserById);
-router.post('/', addUser);
-router.put('/:id', updateUser);
-router.delete('/:id', deleteUser);
-
-module.exports = router;
+module.exports = router
