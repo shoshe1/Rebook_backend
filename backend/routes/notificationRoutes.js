39a89bd0c8b61af12a1387590ddf155dddfedb6a
shoshe1 <119072579+shoshe1@users.noreply.github.com>
2025-03-21 17:21:07 +0200

    Update notificationRoutes.js

--- backend/routes/notificationRoutes.js
+++ backend/routes/notificationRoutes.js
@@ -4,6 +4,7 @@ const {
   getNotifications,
   markNotificationAsRead,
   sendOverdueNotification,
+  updateNotificationStatus
 } = require('../controllers/notificationController');
 
 const router = express.Router();
@@ -15,5 +16,7 @@ router.get('/notifications/:customer_id', getNotifications);
 router.patch('/notifications/:notification_id/mark-as-read', markNotificationAsRead);
 
 router.post('/notifications/send-overdue',sendOverdueNotification);
+
+router.patch('/notifications/:notification_id/status', updateNotificationStatus);
 
 module.exports = router;
